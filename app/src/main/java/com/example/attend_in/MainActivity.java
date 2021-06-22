d0321b4dc37403992990a7a1308073c5b3467efb
Amy Jun <amyjun29@gmail.com>
2021-06-22 11:48:35 -0400

    Only get latitude and longitude upon opening the app Student ID input Toast message after button click

--- app/src/main/java/com/example/attend_in/MainActivity.java
+++ app/src/main/java/com/example/attend_in/MainActivity.java
@@ -13,54 +13,79 @@ import com.android.volley.toolbox.Volley;;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
     Button btn_checkIn;
+    EditText txt_studentID;
 
 
+    //Upon opening the app
+    //Gets the student's geolocation
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //assigning variables
+        //URL for API
+        RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+        final String API = "1f576b29567c4c9493df858dd9c285c5";
+        final String URL_PREFIX = "https://api.ipgeolocation.io/ipgeo?apiKey=";
+        final String URL_POSTFIX = "&ip=1.1.1.1&fields=geo";
+        String url = URL_PREFIX + API + URL_POSTFIX;
+
+        //Get JSON Object
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+
+                //Assigning variables for latitude and longitude
+                String lat = "";
+                String longt = "";
+
+                //Try Catch block for getting JSON object
+                try {
+                    lat = response.getString("latitude");
+                    longt = response.getString("longitude");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                Toast.makeText(MainActivity.this, lat + " " + longt, Toast.LENGTH_SHORT).show();
+            }
+        },
+                //Error listener
+
+                new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(MainActivity.this, "error", Toast.LENGTH_SHORT).show();
+            }
+        });
+        //Add the request for geolocation on to the queue
+        queue.add(request);
+
+
+        //Check in Button
         btn_checkIn = findViewById(R.id.checkInButton);
+        txt_studentID = (EditText) findViewById(R.id.studentID);
 
-
-        //click listeners
         btn_checkIn.setOnClickListener(new View.OnClickListener() {
-
             public void onClick(View view){
+                //Toast message to let the student know they're checked in
+                String studentID = txt_studentID.getText().toString();
+                Toast.makeText(MainActivity.this, studentID + " You're checked In. Thanks!", Toast.LENGTH_SHORT).show();
 
-                RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
-                final String API = "1f576b29567c4c9493df858dd9c285c5";
-                final String URL_PREFIX = "https://api.ipgeolocation.io/ipgeo?apiKey=";
-                String url = URL_PREFIX + API;
-
-                //JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, );
-
-                StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        Toast.makeText(MainActivity.this, response, Toast.LENGTH_SHORT).show();
-
-                    }
-                }, new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Toast.makeText(MainActivity.this, "error", Toast.LENGTH_SHORT).show();
-                    }
-
-                });
-                queue.add(stringRequest);
             }
 
 
         });
+
+
 
 
 
