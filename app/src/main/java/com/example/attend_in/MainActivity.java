a2f05d34ff3de080455f985199475198bda05b22
Amy Jun <amyjun29@gmail.com>
2021-06-22 15:51:27 -0400

    Compare user's location with classroom location Set alerts for student ID Check in button toast message based on location

--- app/src/main/java/com/example/attend_in/MainActivity.java
+++ app/src/main/java/com/example/attend_in/MainActivity.java
@@ -11,6 +11,7 @@ import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -23,6 +24,12 @@ import org.json.JSONObject;
 public class MainActivity extends AppCompatActivity {
     Button btn_checkIn;
     EditText txt_studentID;
+    String lat = "";
+    String longt = "";
+    boolean inClass;
+    //Set classroom location (It's currently set as the user's location for now, but we can change later)
+    String classLat = "47.66878";
+    String classLongt = "-122.12208";
 
 
     //Upon opening the app
@@ -37,17 +44,14 @@ public class MainActivity extends AppCompatActivity {
         RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
         final String API = "1f576b29567c4c9493df858dd9c285c5";
         final String URL_PREFIX = "https://api.ipgeolocation.io/ipgeo?apiKey=";
-        final String URL_POSTFIX = "&ip=1.1.1.1&fields=geo";
+        final String URL_POSTFIX = "&ip=2600:1700:d72:1010:c53f:833:4e8d:e4d1";
         String url = URL_PREFIX + API + URL_POSTFIX;
+        //RISK 1: GETTING STUDENT'S ID ADDRESS AND LOCATION FROM IT MIGHT CONFLICT WITH PRIVACY ISSUES. MIGHT NEED ACCESS PERMISSION
 
         //Get JSON Object
         JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
-
-                //Assigning variables for latitude and longitude
-                String lat = "";
-                String longt = "";
 
                 //Try Catch block for getting JSON object
                 try {
@@ -56,38 +60,70 @@ public class MainActivity extends AppCompatActivity {
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
-                Toast.makeText(MainActivity.this, lat + " " + longt, Toast.LENGTH_SHORT).show();
+                //check to see if the student is within the classroom range
+                for(int i = 0; i<6; i++){
+                    if((lat.charAt(i) == classLat.charAt(i)) && (longt.charAt(i) == classLongt.charAt(i)))
+                        inClass = true;
+                }
+                //RISK 2: THE FIRST 6 DIGITS MIGHT NOT BE ENOUGH TO COMPARE THE CLASSROOM LOCATION AND THE STUDENT'S LOCATION
+
+
+
+                //Toast.makeText(MainActivity.this, lat + " " + longt, Toast.LENGTH_SHORT).show();
             }
         },
                 //Error listener
-
                 new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
                 Toast.makeText(MainActivity.this, "error", Toast.LENGTH_SHORT).show();
             }
+
+
+
         });
         //Add the request for geolocation on to the queue
         queue.add(request);
 
-
         //Check in Button
-        btn_checkIn = findViewById(R.id.checkInButton);
-        txt_studentID = (EditText) findViewById(R.id.studentID);
+            //if the student is within the classroom range
+            if(inClass = true) {
+                btn_checkIn = findViewById(R.id.checkInButton);
+                txt_studentID = (EditText) findViewById(R.id.studentID);
 
-        btn_checkIn.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view){
-                //Toast message to let the student know they're checked in
-                String studentID = txt_studentID.getText().toString();
-                Toast.makeText(MainActivity.this, studentID + " You're checked In. Thanks!", Toast.LENGTH_SHORT).show();
+                btn_checkIn.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View view) {
+                        String studentID = txt_studentID.getText().toString();
 
+                        //set error if the student ID is empty or shorter than 9 digits
+                        if(studentID.equals("") || studentID.length()<9) {
+                            txt_studentID.setError("Please enter your student ID");
+                            return;
+                        }
+                        //Toast message to let the student know they're checked in
+                        Toast.makeText(MainActivity.this, studentID + " You're checked In. Thanks!", Toast.LENGTH_SHORT).show();
+
+                    }
+
+
+                });
             }
+            //if the student is not within the classroom range,
+            else{
+                btn_checkIn = findViewById(R.id.checkInButton);
+                txt_studentID = (EditText) findViewById(R.id.studentID);
+
+                btn_checkIn.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View view) {
+                        //Toast message to let the student know they're checked in
+                        String studentID = txt_studentID.getText().toString();
+                        Toast.makeText(MainActivity.this, "You're not within the classroom range.", Toast.LENGTH_SHORT).show();
+
+                    }
 
 
-        });
-
-
-
+                });
+            }
 
 
     }
