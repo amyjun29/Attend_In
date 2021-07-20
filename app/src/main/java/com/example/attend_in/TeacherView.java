ba755e291e3e372bc740208e91571c43878eafd1
An Nguyen <sfaigon@gmail.com>
2021-07-20 11:04:10 -0400

    teacher view button to switch back

--- app/src/main/java/com/example/attend_in/TeacherView.java
+++ app/src/main/java/com/example/attend_in/TeacherView.java
@@ -2,13 +2,29 @@ package com.example.attend_in;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class TeacherView extends AppCompatActivity {
+    private Button button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teacher_view);
+
+        button2 =(Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMainActivity();
+            }
+        });
+    }
+    public void openMainActivity(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
