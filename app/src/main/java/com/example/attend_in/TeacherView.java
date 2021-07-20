8247f105afec815c8cbee03cbbe1652843dd42c5
An Nguyen <sfaigon@gmail.com>
2021-07-20 12:22:36 -0400

    attempt #1 at display database

--- app/src/main/java/com/example/attend_in/TeacherView.java
+++ app/src/main/java/com/example/attend_in/TeacherView.java
@@ -1,20 +1,34 @@
 package com.example.attend_in;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class TeacherView extends AppCompatActivity {
     private Button button2;
+    private ListView listView;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teacher_view);
-
+        //button
         button2 =(Button) findViewById(R.id.button2);
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -22,9 +36,37 @@ public class TeacherView extends AppCompatActivity {
                 openMainActivity();
             }
         });
+
+        //data display
+        listView = findViewById(R.id.listView);
+        final ArrayList<String> list = new ArrayList<>();
+        ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.list_item,list);
+        listView.setAdapter(adapter);
+
+        //database reference variable
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("Student").child("Mf0v32_HIjhBIigZN_I").child("name:");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                list.clear();
+                for(DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    list.add(snapshot.getValue().toString());
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull  DatabaseError error) {
+
+            }
+        });
+
     }
+    //button stuff
     public void openMainActivity(){
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
+
+
 }
