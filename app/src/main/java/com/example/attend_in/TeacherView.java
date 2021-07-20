d118370dec8921045ac919e64a1b9bb3d94d4825
An Nguyen <sfaigon@gmail.com>
2021-07-20 14:00:55 -0400

    attempt #2 issue with the My.adapter.java, cant use getName and get id

--- app/src/main/java/com/example/attend_in/TeacherView.java
+++ app/src/main/java/com/example/attend_in/TeacherView.java
@@ -1,13 +1,28 @@
 package com.example.attend_in;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
 public class TeacherView extends AppCompatActivity {
+    RecyclerView recyclerView;
+    DatabaseReference database;
+    MyAdapter myAdapter;
+    ArrayList<Student> list;
     private Button button2;
 
     @Override
@@ -22,9 +37,37 @@ public class TeacherView extends AppCompatActivity {
                 openMainActivity();
             }
         });
+        // list of student
+        recyclerView = findViewById(R.id.userList);
+        database = FirebaseDatabase.getInstance().getReference("Student");
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        list= new ArrayList<>();
+        myAdapter = new MyAdapter(this , list);
+        recyclerView.setAdapter(myAdapter);
+
+        database.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                for(DataSnapshot dataSnapshot : snapshot.getChildren()){
+                    Student user = dataSnapshot.getValue(Student.class);
+                    list.add(user);
+                }
+                myAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
     }
     public void openMainActivity(){
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
+
 }
